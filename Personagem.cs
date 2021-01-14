8bd40cbb27c65a5fd5adc872c8288d55affe17d0
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2021-01-14 00:46:50 -0200

    Exercicio prático

---
+++ Personagem.cs
@@ -0,0 +1,11 @@
+namespace POO_Personagem
+{
+    public class Personagem
+    {
+        public string nome;
+        public int idade;
+        public string armadura;
+        public string IA;
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace POO_Personagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Regis crie um programa que mostre na tela dados de atributos de objetos diferentes
+            Personagem heroi1 = new Personagem();
+            Console.WriteLine("Digite o nome do Personagem: ");
+            heroi1.nome = Console.ReadLine();
+
+            Personagem heroi2 = new Personagem();
+            Console.WriteLine("Digite o nome do Personagem: ");
+            heroi2.nome = Console.ReadLine();
+
+            Console.WriteLine($"Os nomes dos herois são {heroi1.nome} e {heroi2.nome}");
+
+        }
+    }
+}
